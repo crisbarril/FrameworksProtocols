6680d9296c5e3b40e956855f515f3f8b2095385f
Cristian Barril <cbarril@opentrends.net>
2018-09-05 16:29:00 +0200

    RENAME from FrameworksProtocols to Gateway

---
+++ Gateway/Gateway.h
@@ -0,0 +1,19 @@
+//
+//  Gateway.h
+//  Gateway
+//
+//  Created by Cristian Barril on 05/09/2018.
+//  Copyright © 2018 Cristian Barril. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Gateway.
+FOUNDATION_EXPORT double GatewayVersionNumber;
+
+//! Project version string for Gateway.
+FOUNDATION_EXPORT const unsigned char GatewayVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Gateway/PublicHeader.h>
+
+
